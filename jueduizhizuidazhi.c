322e220e5d63084a99ea9e6971e5d2e6964e2aca
acupofa <a2874747006@gmail.com>
2020-11-15 23:10:01 +0800

    没学完

---
+++ jueduizhizuidazhi.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+  int i,a,b;
+  int max=0;
+  scanf("%d",&i);
+  for (a=1;a<i;a++)
+  {
+    scanf("%d",&b);
+    if (a<0)
+    {
+      a=-a;
+      max=a;
+    }
+    else max=a;
+    if ()
+    {
+      /* code */
+    }
+    
+  }
+}
